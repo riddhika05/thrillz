d0453dce406d73742ae5bae2ffe8d51e27e22bff
Riddhika Arora <riddhika05@users.noreply.github.com>
2025-08-13 00:51:29 +0530

    navigating to explore page on clicking exploremap

--- thrillz/src/pages/Post.jsx
+++ thrillz/src/pages/Post.jsx
@@ -1,43 +1,47 @@
-import React from 'react';
-import './Post.css';
+import React from "react";
+import "./Post.css";
 
 // Import asset images (update paths as needed)
-import heartIcon from '../assets/heart.png';
-import commentIcon from '../assets/comment.png';
-import shareIcon from '../assets/share.png';
-import uploadIcon from '../assets/upload.png';
-import musicIcon from '../assets/music.png';
-import botIcon from '../assets/bot.png';
-import profileAvatar from '../assets/profile.png';
-import backgroundImg from '../assets/post.png';
+import heartIcon from "../assets/heart.png";
+import commentIcon from "../assets/comment.png";
+import shareIcon from "../assets/share.png";
+import uploadIcon from "../assets/upload.png";
+import musicIcon from "../assets/music.png";
+import botIcon from "../assets/bot.png";
+import profileAvatar from "../assets/profile.png";
+import backgroundImg from "../assets/post.png";
+import { useNavigate } from "react-router-dom";
 
 function Post() {
   return (
-   <div className='Post_Page'>
-    <Header/>
-   </div>
+    <div className="Post_Page">
+      <Header />
+    </div>
   );
 }
-function Header()
-{
-  return(
-   <div className="Header">
-       <img src={profileAvatar} alt="Profile Avatar" className="profile"/>
-       <div className="right">
-       <img src={musicIcon} alt="Music" className="music"/>
-       <Explore/>
-       </div>
-   </div>
-  )
+function Header() {
+  return (
+    <div className="Header">
+      <img src={profileAvatar} alt="Profile Avatar" className="profile" />
+      <div className="right">
+        <img src={musicIcon} alt="Music" className="music" />
+        <Explore />
+      </div>
+    </div>
+  );
 }
 
-function Explore()
-{
-  return(
-    <div className="explore">
+function Explore() {
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/explore");
+  }
+  return (
+    <div className="explore" onClick={handleClick}>
       <div className="explore_text">Explore Map</div>
-      </div>
-  )
+    </div>
+  );
 }
 
 export default Post;
