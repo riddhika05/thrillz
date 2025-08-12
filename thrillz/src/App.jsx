b9dcab81c70496c759fae019dfe6d1c0691ede04
Riddhika Arora <riddhika05@users.noreply.github.com>
2025-08-12 17:00:18 +0530

    Added routing

--- thrillz/src/App.jsx
+++ thrillz/src/App.jsx
@@ -1,4 +1,11 @@
 import Home from './pages/Home';
+import Post from './pages/Post';
+import Map from './pages/Map';
+import Profile from './pages/Profile';
+import EditProfile from './pages/EditProfile';
+import Avatar from './pages/Avatar';
+import Chatbot from './pages/Chatbot';
+import Chat from './pages/Chat';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   
@@ -8,6 +15,14 @@ function App() {
      <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/post' element={<Post />} />
+        <Route path='/map' element={<Map />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/edit-profile' element={<EditProfile />} />
+        <Route path='/avatar' element={<Avatar />} />
+        <Route path='/chatbot' element={<Chatbot />} />
+        <Route path='/chat' element={<Chat />} />
+        
       </Routes>
      </Router>
     </>
