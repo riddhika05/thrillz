586b8edcc679afde746372c0dba6b016acf52221
unknown <23ucs512@lnmiit.ac.in>
2025-08-14 11:30:44 +0530

    commit 1

--- thrillz/src/pages/EditProfile.jsx
+++ thrillz/src/pages/EditProfile.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
 const EditProfile = () => {
+  const [nickname, setNickname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [triggerWordInput, setTriggerWordInput] = useState('');
+  const [triggerWords, setTriggerWords] = useState([]);
+  const [profanity, setProfanity] = useState(true);
+  
   return (
     <div>EditProfile</div>
   )
