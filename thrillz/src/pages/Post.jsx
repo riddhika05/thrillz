19708e499325fbb010f071825f1c0066187a5c70
Riddhika Arora <riddhika05@users.noreply.github.com>
2025-08-15 14:27:35 +0530

    Removed Scrollbar

--- thrillz/src/pages/Post.jsx
+++ thrillz/src/pages/Post.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import musicIcon from "../assets/music.png";
 import botIcon from "../assets/bot.png";
 import profileAvatar from "../assets/profile.png";
-import Whispers from "./Whispers"
+import Whispers from "./Whispers";
 import { useNavigate } from "react-router-dom";
-import postBackground from '../assets/post.png'; 
+import postBackground from "../assets/post.png";
 function Post() {
   return (
-  
-    <div className="w-screen bg-cover bg-no-repeat bg-center bg-fixed min-h-screen m-0" style={{ backgroundImage: `url(${postBackground})` }}>
+    <div
+      className="w-screen bg-cover bg-no-repeat bg-center bg-fixed min-h-screen m-0"
+      style={{ backgroundImage: `url(${postBackground})` }}
+    >
       <Header />
     </div>
   );
@@ -27,24 +29,31 @@ function Header() {
 
   return (
     <>
-      
       <div className="sticky top-0 left-0 p-8 flex items-center z-10">
-      
-        <img src={profileAvatar} alt="Profile Avatar" className="h-16 w-16 md:h-20 md:w-20 cursor-pointer" onClick={handleClick} />
-        
+        <img
+          src={profileAvatar}
+          alt="Profile Avatar"
+          className="h-16 w-16 md:h-20 md:w-20 cursor-pointer"
+          onClick={handleClick}
+        />
+
         <div className="ml-auto flex items-center gap-20 text-[#5a4fcf]">
-        
           <img src={musicIcon} alt="Music" className="h-28 md:h-32" />
           <Explore />
         </div>
       </div>
-    
-      <div className="w-full sm:w-3/4 lg:w-1/2 mx-auto mt-12 max-h-[70vh] overflow-y-auto">
-       <Whispers />
-     </div>
+
+      <div className="w-full sm:w-3/4 lg:w-1/2 mx-auto mt-12 max-h-[70vh] overflow-y-auto hide-scrollbar">
+        <Whispers />
+      </div>
+
       <div className="fixed bottom-8 right-8 z-50">
-       
-        <img src={botIcon} alt="Bot" className="h-16 w-16 md:h-20 md:w-20 cursor-pointer" onClick={handleClickBot} />
+        <img
+          src={botIcon}
+          alt="Bot"
+          className="h-16 w-16 md:h-20 md:w-20 cursor-pointer"
+          onClick={handleClickBot}
+        />
       </div>
     </>
   );
@@ -56,12 +65,15 @@ function Explore() {
   function handleClick() {
     navigate("/explore");
   }
-  
-  return (
-    
-    <div className="w-40 h-20 bg-[#D9D9D9] rounded-[40px] flex cursor-pointer" onClick={handleClick}>
 
-      <div className="relative left-8 top-6 font-['Pacifico'] font-normal not-italic">Explore Map</div>
+  return (
+    <div
+      className="w-40 h-20 bg-[#D9D9D9] rounded-[40px] flex items-center justify-center cursor-pointer"
+      onClick={handleClick}
+    >
+      <div className="font-['Pacifico'] font-normal not-italic text-center">
+        Explore Map
+      </div>
     </div>
   );
 }
