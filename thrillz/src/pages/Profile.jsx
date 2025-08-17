c4d2ef2aea0a5d5303ac8645d50fba3f8fc35b7e
Riddhika Arora <riddhika05@users.noreply.github.com>
2025-08-17 11:40:27 +0530

    Added basic UI for profile

--- thrillz/src/pages/Profile.jsx
+++ thrillz/src/pages/Profile.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from 'react';
+import myImage from "../assets/profile_bkg.png";
+import { FaArrowLeft, FaShareAlt } from "react-icons/fa";
+import "./Profile.css";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <fragment>
+     <div  >
+     {/* Top Navigation */}
+      <div className="top-bar">
+  <FaArrowLeft className="icon" />
+  <FaShareAlt className="icon" />
+</div>
+
+     
+<div className="profile-header">
+  <div className="profile-pic">
+    <img src={myImage} alt="Profile" />
+  </div>
+
+  <div className="stats">
+    <div className="stat">
+      <span className="stat-number">2k</span>
+      <span className="stat-label">points</span>
+    </div>
+    <div className="stat">
+      <span className="stat-number">2k</span>
+      <span className="stat-label">likes</span>
+    </div>
+    <div className="stat">
+      <span className="stat-number">2k</span>
+      <span className="stat-label">posts</span>
+    </div>
+  </div>
+</div>
+
+
+
+
+       
+
+ <div className='edit-profile'>
+          <button className='buttoneditprof'>Edit Profile</button>
+        </div>
+
+         
+
+
+       {/* Recently Viewed */}
+      <h2 className="section-title">Posts</h2>
+      <div className="recently-viewed">
+        {[...Array(4)].map((_, i) => (
+          <div key={i} className="square-box"></div>
+        ))}
+      </div>
+
+   
+    </div>
+
+
+ 
+</fragment>
+
+
+
+
+)};
+
+
+ 
 
 export default Profile
