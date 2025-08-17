766325b4a31c42116167d2f538c3b408dff52cfa
Riddhika Arora <riddhika05@users.noreply.github.com>
2025-08-17 13:01:34 +0530

    Resolved the white margins issue by removing the body tag from profile

--- thrillz/src/pages/Profile.jsx
+++ thrillz/src/pages/Profile.jsx
@@ -1,73 +1,51 @@
 import React from 'react';
-import myImage from "../assets/profile_bkg.png";
+import myImage from "../assets/girl.png";
 import { FaArrowLeft, FaShareAlt } from "react-icons/fa";
 import "./Profile.css";
 
 const Profile = () => {
   return (
-    <fragment>
-     <div  >
-     {/* Top Navigation */}
+    <>
+      {/* Top Navigation */}
       <div className="top-bar">
-  <FaArrowLeft className="icon" />
-  <FaShareAlt className="icon" />
-</div>
+        <FaArrowLeft className="icon" />
+        <FaShareAlt className="icon" />
+      </div>
 
-     
-<div className="profile-header">
-  <div className="profile-pic">
-    <img src={myImage} alt="Profile" />
-  </div>
-
-  <div className="stats">
-    <div className="stat">
-      <span className="stat-number">2k</span>
-      <span className="stat-label">points</span>
-    </div>
-    <div className="stat">
-      <span className="stat-number">2k</span>
-      <span className="stat-label">likes</span>
-    </div>
-    <div className="stat">
-      <span className="stat-number">2k</span>
-      <span className="stat-label">posts</span>
-    </div>
-  </div>
-</div>
-
-
-
-
-       
-
- <div className='edit-profile'>
-          <button className='buttoneditprof'>Edit Profile</button>
+      <div className="profile-header">
+        <div className="profile-pic">
+          <img src={myImage} alt="Profile" />
         </div>
 
-         
+        <div className="stats">
+          <div className="stat">
+            <span className="stat-number">2k</span>
+            <span className="stat-label">points</span>
+          </div>
+          <div className="stat">
+            <span className="stat-number">2k</span>
+            <span className="stat-label">likes</span>
+          </div>
+          <div className="stat">
+            <span className="stat-number">2k</span>
+            <span className="stat-label">posts</span>
+          </div>
+        </div>
+      </div>
 
+      <div className="edit-profile">
+        <button className="buttoneditprof">Edit Profile</button>
+      </div>
 
-       {/* Recently Viewed */}
+      {/* Recently Viewed */}
       <h2 className="section-title">Posts</h2>
       <div className="recently-viewed">
         {[...Array(4)].map((_, i) => (
           <div key={i} className="square-box"></div>
         ))}
       </div>
+    </>
+  );
+};
 
-   
-    </div>
-
-
- 
-</fragment>
-
-
-
-
-)};
-
-
- 
-
-export default Profile
+export default Profile;
