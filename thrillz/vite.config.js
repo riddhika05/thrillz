4067bb401250f7c62d5db99c47fcff24c73caaea
Riddhika Arora <riddhika05@users.noreply.github.com>
2025-08-25 19:52:01 +0530

    Changed vite.config.js

--- thrillz/vite.config.js
+++ thrillz/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist', // default, but good to be explicit
+  },
+  server: {
+    port: 3000,     // optional, for local dev
+  }
 })
